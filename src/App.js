859ce3eac043b8cc945008e1e8cd7cf8541bc528
unknown <michael.h@patrianna.com>
2024-07-08 14:38:38 +0200

    Make access manager work

--- src/App.js
+++ src/App.js
@@ -1,66 +1,56 @@
-import React, { useEffect, useState } from "react";
+/* Imports PubNub JavaScript and React SDKs to create and access PubNub instance across your app. */
+/* Imports the required PubNub Chat Components to easily create chat apps with PubNub. */
+import React from "react";
 import PubNub from "pubnub";
-import { PubNubProvider, usePubNub } from "pubnub-react";
+import { PubNubProvider } from "pubnub-react";
 import { Chat, MessageList, MessageInput } from "@pubnub/react-chat-components";
 
-// Assume this comes from your authentication system
-const authenticatedUser = {
-  id: "user1",
-  name: "Alice",
-  authToken: "user1-auth-token"
-};
-
+/* Create PubNub instance*/
 const pubnub = new PubNub({
   publishKey: 'pub-c-dc6b6d35-b1b6-45c4-8a0a-5ce8cd792219',
   subscribeKey: 'sub-c-727207b9-420e-4e21-b1ff-ff57ea44f2df',
-  userId: authenticatedUser.id,
-  authKey: authenticatedUser.authToken,
+  secretKey: 'sec-c-ZTBjZGEyMmYtOTc1Yy00ZWNkLTlkZGEtODBkYmUzMGYzZmVm',
+  userId: "myFirstUser",
 });
 
-const currentChannel = "Default";
+const currentChannel = "default";
+
+/* Get an access token to work with Access Manager: In production this would run on the server */
+pubnub.grantToken(
+  {
+    ttl: 15,
+    authorized_uuid: "my-authorized-uuid",
+    resources: {
+      channels: {
+        currentChannel: {
+          read: true,
+          write: true
+        }
+      }
+    }
+  },
+  function (status, token) {
+    console.log("token received:" + token)
+  });
+
+
 const theme = "light";
 
-function ChatComponent() {
-  const pubnub = usePubNub();
-  const [hasPostedHello, setHasPostedHello] = useState(false);
-
-  useEffect(() => {
-    pubnub.subscribe({ channels: [currentChannel] });
-
-    // Fake user posts "Hello Chat"
-    if (!hasPostedHello) {
-      const fakeUser = {
-        id: "fakeUser",
-        name: "Fake User"
-      };
-
-      pubnub.publish({
-        channel: currentChannel,
-        message: {
-          type: "text",
-          text: "Hello Chat!",
-          sender: fakeUser,
-        },
-      }).then(() => setHasPostedHello(true));
-    }
-
-    return () => {
-      pubnub.unsubscribe({ channels: [currentChannel] });
-    };
-  }, [pubnub, hasPostedHello]);
-
-  return (
-    <Chat currentChannel={currentChannel} theme={theme}>
-      <MessageList />
-      <MessageInput />
-    </Chat>
-  );
-}
-
+/* Display the chat */
 function App() {
   return (
     <PubNubProvider client={pubnub}>
-      <ChatComponent />
+      {/* PubNubProvider is a part of the PubNub React SDK and allows you to access PubNub instance
+      in components down the tree. */}
+      <Chat {...{ currentChannel, theme }}>
+        {/* Chat is an obligatory state provider. It allows you to configure some common component
+        options, like the current channel and the general theme for the app. */}
+          <MessageList 
+          fetchMessages={100} // Fetch the last 100 messages
+                  
+        ></MessageList>
+        <MessageInput />
+      </Chat>
     </PubNubProvider>
   );
 }
